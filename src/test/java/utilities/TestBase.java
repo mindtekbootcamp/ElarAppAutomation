eb41cd4b60bff5b426d17db6232311deb41dacea
mindtekbatch14 <training@mindtek.io>
2025-09-19 13:41:01 -0500

    framework setup

---
+++ src/test/java/utilities/TestBase.java
@@ -0,0 +1,21 @@
+package utilities;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class TestBase {
+
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setup(){
+        driver = Driver.getDriver();
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
+
+}
